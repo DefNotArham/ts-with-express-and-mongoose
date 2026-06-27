ff09d13e3601640e0782924b7161a3746f4809ec
DefNotArham <arhamkabir231@gmail.com>
2026-06-27 11:38:31 -0400

    connnectDb

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import connectDb from "./db/connectDb.js";
+
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
+
+connectDb();
 
 app.listen(port, () => console.log(`Server is running on ${port}`));
